e19a7f67c6f56fd1757dc84ef84758d1b2ed22d6
git-package <githubpackages@outlook.com>
2024-12-06 08:55:32 -0600

    Create num-13.py

---
+++ num-13.py
@@ -0,0 +1,5 @@
+number = int(input("Please enter the number you want to check: "))
+if number == 13:
+    print("yes, the number is equal to 13.")
+else:
+    print("no, the number is not equal to 13.")
